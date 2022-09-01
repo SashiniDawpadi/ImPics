f64ded701ded9482392fef3ed1ad12d2cc4a25fe
Sashini Dawpadi <86653321+SashiniDawpadi@users.noreply.github.com>
2022-09-01 13:11:17 +0530

    modules | components created

---
+++ src/app/home-page/home-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
+
+
+
+@NgModule({
+  declarations: [
+    PhotoGalleryComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomePageModule { }
