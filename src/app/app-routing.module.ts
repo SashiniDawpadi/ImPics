7887f9ac8171b71e45d47a371425a03877d3d8a8
Sashini Dawpadi <86653321+SashiniDawpadi@users.noreply.github.com>
2022-12-29 07:02:42 +0530

    files added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './authentication/sign-up/sign-up.component';
 import { PhotoGalleryComponent } from './home-page/photo-gallery/photo-gallery.component';
 
 const routes: Routes = [
-  {path: '',component: PhotoGalleryComponent}
+  { path: '', component: PhotoGalleryComponent },
+  { path: 'signup', component: SignUpComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoGalleryComponent } from './photo-gallery/photo-gallery.component';
+import { HomePageComponent } from './home-page.component';
 
 
 
 @NgModule({
   declarations: [
-    PhotoGalleryComponent
+    PhotoGalleryComponent,
+    HomePageComponent
   ],
   imports: [
     CommonModule
